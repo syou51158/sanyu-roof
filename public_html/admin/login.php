bc528ac691cedd8026b20835e95202d46baf96b3
村岡翔 <example@example.com>
2026-01-27 00:08:31 +0900

    Enable debug mode in admin/login.php

--- public_html/admin/login.php
+++ public_html/admin/login.php
@@ -1,5 +1,10 @@
 <?php
 // public_html/admin/login.php
+// Debug settings
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once '../config/config.php';
 
 $error = '';
